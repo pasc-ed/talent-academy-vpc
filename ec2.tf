cf6fb11be9faaa7a2473067a0f4db2217a809b9c
pascal.edouard@brambles.com <pascal@edouard.tech>
2021-12-06 21:19:13 +0000

    ignore ec2

--- ec2.tf
+++ ec2.tf
@@ -33,32 +33,32 @@ resource "aws_security_group" "my_app_sg" {
   }
 }
 
-data "aws_ami" "my_aws_ami" {
-    owners = ["137112412989"]
-    most_recent = true
-    filter {
-        name = "name"
-        values = [ "amzn2-ami-kernel-*" ]
-    }
-}
+# data "aws_ami" "my_aws_ami" {
+#     owners = ["137112412989"]
+#     most_recent = true
+#     filter {
+#         name = "name"
+#         values = [ "amzn2-ami-kernel-*" ]
+#     }
+# }
 
 # EC2 - PUBLIC
-resource "aws_instance" "my_public_server" {
-    ami = data.aws_ami.my_aws_ami.id
-    instance_type = var.instance_type
-    key_name = var.keypair_name
-    subnet_id = aws_subnet.public_a.id
-    vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
-}
+# resource "aws_instance" "my_public_server" {
+#     ami = data.aws_ami.my_aws_ami.id
+#     instance_type = var.instance_type
+#     key_name = var.keypair_name
+#     subnet_id = aws_subnet.public_a.id
+#     vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
+# }
 
-# EC2 - PRIVATE
-resource "aws_instance" "my_first_server" {
-    ami = data.aws_ami.my_aws_ami.id
-    instance_type = var.instance_type
-    key_name = var.keypair_name
-    subnet_id = aws_subnet.private_a.id
-    vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
-}
+# # EC2 - PRIVATE
+# resource "aws_instance" "my_first_server" {
+#     ami = data.aws_ami.my_aws_ami.id
+#     instance_type = var.instance_type
+#     key_name = var.keypair_name
+#     subnet_id = aws_subnet.private_a.id
+#     vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
+# }
 
 # AMI ID
 # INSTANCE TYPE
