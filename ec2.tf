3c475a92a16c22d1a17a8d6644ccd0d93cd76112
pascal.edouard@brambles.com <pascal@edouard.tech>
2021-12-08 10:54:12 +0000

    UPDATE - ec2 instance resource

--- ec2.tf
+++ ec2.tf
@@ -43,13 +43,13 @@ data "aws_ami" "my_aws_ami" {
 }
 
 # EC2 - PUBLIC
-# resource "aws_instance" "my_public_server" {
-#     ami = data.aws_ami.my_aws_ami.id
-#     instance_type = var.instance_type
-#     key_name = var.keypair_name
-#     subnet_id = aws_subnet.public_a.id
-#     vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
-# }
+resource "aws_instance" "my_public_server" {
+    ami = data.aws_ami.my_aws_ami.id
+    instance_type = var.instance_type
+    key_name = var.keypair_name
+    subnet_id = module.network.public_subnet_a_id
+    vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
+}
 
 # # EC2 - PRIVATE
 # resource "aws_instance" "my_first_server" {
